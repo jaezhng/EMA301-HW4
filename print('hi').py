99d96d5b070e5e3a91fdad4d8714811c2e4c139f
jaezhng <158227018+jaezhng@users.noreply.github.com>
2025-03-26 11:22:14 -0400

    modified answer

--- print('hi').py
+++ print('hi').py
@@ -5,11 +5,11 @@ intens = 10
 t = 1
 props = ['Index of Refraction, n', 'Absorption Coefficient (m^-1)', 'Thickness (m)']
 a = [
-    ["1", "1.489", "1.333", "1.460","1.489"],
-    ["0", "0.132", "0.097", "0.132","0.132"],
-    ["0", "0.02", "0.03", "0.025","0.02"]
+    ["1", "1.489", "1.333", "1.460","1.489","1"],
+    ["0", "0.132", "0.097", "0.132","0.132","0"],
+    ["0", "0.02", "0.03", "0.025","0.02","0"]
 ]
-df = pd.DataFrame(a, index=props, columns= ['Air','PMMA', 'Water', 'New Polymer','PMMA'])
+df = pd.DataFrame(a, index=props, columns= ['Air','PMMA', 'Water', 'New Polymer','PMMA','Air'])
 print(df)
 value = df.iloc[0,0]
 print(value)
@@ -24,12 +24,13 @@ float(df.iloc[0,0]) + float(df.iloc[0,1])
 #result = g2 + g1
 #print(result)
 #t represents number of trials
-while t < 5:
+while t < 6:
     R = ((float(df.iloc[0,t]) - float(df.iloc[0,t-1]))/(float(df.iloc[0,t]) + float(df.iloc[0,t-1])))
     R2 = R**2
     print(R2)
     intens = float(intens) * (1-R2)**2 * np.exp(float(df.iloc[1,t])*float(df.iloc[2,t])*-1)
     print(intens)
     t = t + 1
+
 print("Final Transmitted Light Intensity (W/m^2):")
 print(intens)
